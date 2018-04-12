b37021b2f98f49c5a6f879a9771f64356e4495fc
Jacob Christensen <jacob.christensen@starticket.ch>
2018-04-12 14:56:01 +0200

    Update counter

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -54,5 +54,6 @@ export class HomeComponent implements OnInit {
   removeItem(i) {
     this.goals.splice(i, 1);
     this._data.changeGoal(this.goals);
+    this.itemCount = this.goals.length;
   }
 }
